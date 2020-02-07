1b712d8a86a8627f2476c79a4791a99a5db546f3
nicolasrosa <nicolas.rosa@hotmail.com>
2020-02-07 15:40:29 -0300

    Backup 2020-02-07

--- ros_catkin_ws/build/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nicolas/MEGA/workspace/FCRN-DepthPrediction/ros_catkin_ws/src/examples
-# Build directory: /home/nicolas/MEGA/workspace/FCRN-DepthPrediction/ros_catkin_ws/build/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
